1aa834a16438a89616350fdc3d966d37d53fad39
Pepe Millan <jose.millan@iesdonana.org>
2019-02-06 17:22:31 +0100

    añado apuntes, y apartados a las tareas JQuery

---
+++ Tema7/Actividades/2/ejerciciosJQ.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  //a
+  // $("p:contains('Primero') + ul").css({'fontFamily': 'helvetica', 'color': 'red'});
+
+  //b
+  // $("a:contains('Sistemas Informáticos')").css({'fontFamily': 'helvetica', 'color': 'red'});
+
+  //c
+  // $("li:contains('Sistemas Informáticos')").next().find("a:first").css({'fontFamily': 'helvetica', 'color': 'red'});
+
+  // d
+  // $("li:contains('Sistemas Informáticos') ~ li").css({'fontFamily': 'helvetica', 'color': 'red'});
+
+  //e
+  // $("[href]").css({'fontFamily': 'helvetica', 'color': 'red'});
+
+  //f
+  
+});
